bdb4fd368b03b2c17f63d3560bb68cee82c4fcbc
Lee Olney <leeolney3@gmail.com>
2022-11-16 15:41:47 +0400

    add files

--- 2022/scripts/16_MInimal.R
+++ 2022/scripts/16_MInimal.R
@@ -22,17 +22,17 @@ primary_ne = st_intersection(primary, ne_states)
 
 # Map
 ggplot() +
-  geom_sf(data=ne, fill="#003844", color="#0D1B29") +
-  geom_sf(data=primary_ne, color="white") +
+  geom_sf(data=ne, fill="#1A3C40", color="#193140") +
+  geom_sf(data=primary_ne, color="white", size=.4) +
   coord_sf(expand=FALSE) +
   theme_void() +
-  theme(plot.margin=margin(0,1,.3,1,unit="cm"),
-        plot.background=element_rect(fill="#0D1B29", color=NA),
-        plot.title=element_text(family=f1, color="white", face="bold",vjust=-5, size=15),
+  theme(plot.margin=margin(.1,1,.3,1,unit="cm"),
+        plot.background=element_rect(fill="#193140", color=NA),
+        plot.title=element_text(family=f1, color="white", face="bold",vjust=-4.5, size=13),
         plot.caption = element_text(color="grey90", hjust=0, size=8.5,family=f2, margin=margin(t=15))
   ) +
-  labs(title="Primary roads in the Northeast region",
-       caption="#30DayMapChallange Day 16: Minimal | Source: US Census via {tigris} R package")
+  labs(title="Primary roads in the Northeast census region",
+       caption="#30DayMapChallange Day 16: Minimal | Source: US Census via {tigris} R package") 
 
 ggsave("16_Minimal.png", height=7, width=8)
 
